a77ffeef313e268fcbee53fd812c8390f4c7177e
amir <amir.code98@gmail.com>
2023-11-18 16:19:45 +0330

    Add Ceii-Action section

---
+++ src/components/Main/Cell-Action/Cell-Action.js
@@ -0,0 +1,31 @@
+import './Cell-Action.css'
+
+
+const Cell_Action = () => {
+    return (
+        <section className="cell-action">
+            <div className="cell-action_container">
+                <h2>
+                    ما دوست داریم راه حل های عالی
+                    برای کسب و کارتان ایجاد کنیم
+                </h2>
+                <p>
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از
+                    صنعت چاپ و با استفاده از طراحان گرافیک است.
+                    چاپگرها و متون بلکه روزنامه و مجله در ستون
+                    و سطرآنچنان که لازم است و برای شرایط فعلی
+                    تکنولوژی مورد نیاز و کاربردهای متنوع
+                    با هدف بهبود ابزارهای کاربردی می باشد.
+                </p>
+                <div className="cell-btn">
+                    <a href="#">
+                        شروع کنید
+                    </a>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+
+export default Cell_Action
